565073d330291a2538f6643cf92d8c58e6c3c03d
noman <man@tz.world>
2016-06-22 20:22:57 +0800

    f

--- baidu_transalte.py
+++ baidu_transalte.py
@@ -5,13 +5,22 @@ from config import LANG, PATH
 from _util import walk_md
 from os.path import join
 
+
+def transalte_po(lang, po):
+    li = []
+    for i in po:
+        if not i.msgstr:
+            li.append(i.msgid.strip())
+    print(li[0])
+    input()
+
 def main():
     for dirpath, md_li in walk_md():
         for i in md_li:
             for lang in LANG:
                 f = join(PATH, "po", lang, dirpath, i)
-                print(f)
-                #po = polib.pofile()
+                po = polib.pofile(f[:-2] + "po")
+                transalte_po(lang.split("-")[0], po)
 
 
 if __name__ == "__main__":
